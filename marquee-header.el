77e4becd8a812377eb219c77641a22a77b4fdfef
JenChieh <jcs090218@gmail.com>
2019-10-17 18:17:00 +0800

    Update dep, require 25.1 above.

--- marquee-header.el
+++ marquee-header.el
@@ -7,7 +7,7 @@
 ;; Description: Code interface for displaying marquee in header.
 ;; Keyword: animation header interface library marquee
 ;; Version: 0.0.8
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "25.1"))
 ;; URL: https://github.com/jcs090218/marquee-header
 
 ;; This file is NOT part of GNU Emacs.
