d8e83b837bacdd45d274be42fe2d172fd1cbbba2
jenchieh <jcs090218@gmail.com>
2020-07-20 18:34:12 +0800

    Bump version.

--- marquee-header.el
+++ marquee-header.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Code interface for displaying marquee in header.
 ;; Keyword: animation header interface library marquee
-;; Version: 0.0.8
+;; Version: 0.0.9
 ;; Package-Requires: ((emacs "25.1"))
 ;; URL: https://github.com/jcs-elpa/marquee-header
 
